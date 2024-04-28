98c972cb40aeb45a5fe7c57ddf65e2fe4582b7af
Zakir Aghayev <agayevzakir239@gmail.com>
2024-04-28 14:44:01 +0400

    API added

--- config/urls.py
+++ config/urls.py
@@ -38,5 +38,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apps.tables.apis.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('api-docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api-docs/', schema_view.with_ui('swagger', cache_timeout=0), name='api-docs'),
 ]
