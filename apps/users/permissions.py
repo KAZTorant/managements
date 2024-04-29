21b1978542293130ec5b444082a0b1a2eea25f9b
Zakir Aghayev <agayevzakir239@gmail.com>
2024-04-29 21:07:53 +0400

    Permissions added

---
+++ apps/users/permissions.py
@@ -0,0 +1,22 @@
+from rest_framework import permissions
+
+
+class IsWaitress(permissions.BasePermission):
+    message = 'You must be a waitress to access this endpoint.'
+
+    def has_permission(self, request, view):
+        return request.user.type == 'waitress'
+
+
+class IsAdmin(permissions.BasePermission):
+    message = 'You must be an admin to access this endpoint.'
+
+    def has_permission(self, request, view):
+        return request.user.type == 'admin'
+
+
+class IsRestaurantOwner(permissions.BasePermission):
+    message = 'You must be a restaurant owner to access this endpoint.'
+
+    def has_permission(self, request, view):
+        return request.user.type == 'restaurant'
