6b2d6ab0806dc4a0d6f16882c54c805836851d1c
Zakir Aghayev <agayevzakir239@gmail.com>
2024-05-01 01:17:01 +0400

    fixed

--- config/settings.py
+++ config/settings.py
@@ -153,6 +153,16 @@ SWAGGER_SETTINGS = {
 
 AUTH_USER_MODEL = 'users.User'
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
 
 # CORS_ALLOWED_ORIGINS = [
 #     'https://example.com',
