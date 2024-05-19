bfbe59e01f36ab4b6a95e943e8f2dc2d98f964c3
Zakir Aghayev <agayevzakir239@gmail.com>
2024-05-19 19:00:56 +0400

    z-check

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -9,6 +9,7 @@ from django.http import HttpResponseRedirect
 from django.http import JsonResponse
 from django.utils import timezone
 from django.db.models import Sum
+from django.urls import reverse
 
 import datetime
 
@@ -46,6 +47,8 @@ class StatisticsAdmin(admin.ModelAdmin):
     def get_urls(self):
         urls = super().get_urls()
         custom_urls = [
+            path('calculate-till-now/', self.admin_site.admin_view(self.calculate_till_now),
+                 name='orders_statistics_calculate_till_now'),
             path('calculate-daily/', self.admin_site.admin_view(self.calculate_daily_stats),
                  name='orders_statistics_calculate_daily'),
             path('calculate-monthly/', self.admin_site.admin_view(self.calculate_monthly_stats),
@@ -83,8 +86,24 @@ class StatisticsAdmin(admin.ModelAdmin):
         self.message_user(request, "İllik statistika uğurla əlavə edildi.")
         return HttpResponseRedirect("../")
 
+    def calculate_till_now(self, request):
+        Statistics.objects.calculate_till_now()
+        self.message_user(
+            request, "Bu günə kimi olan statistika uğurla əlavə edildi."
+        )
+        return HttpResponseRedirect("../")
+
     def z_check(self, obj):
         Statistics.objects.delete_orders_for_statistics_day(obj.date)
+        try:
+            PrinterService().send_to_printer(text=obj.print_check)
+        except Exception as e:
+            print("Exception", e)
+            return False
+        return True
+
+    def z_check_till_now(self, obj):
+        Statistics.objects.delete_orders_till_now()
         try:
             PrinterService().send_to_printer(text=obj.print_check)
         except Exception as e:
@@ -95,7 +114,14 @@ class StatisticsAdmin(admin.ModelAdmin):
     def response_change(self, request, obj):
         if "_z-cek" in request.POST:
             self.z_check(obj=obj)
-            self.message_user(request, "Z-Çek processed for %s." % obj.date)
+            self.message_user(request, "Z-Çek hazırlandı %s." % obj.date)
+            # Optionally redirect or perform additional actions
+            return HttpResponseRedirect(".")
+
+        if "_z-cek-till-now" in request.POST:
+            self.z_check_till_now(obj=obj)
+            self.message_user(
+                request, "Z-Çek bu günə kimi hazırlandı %s." % obj.date)
             # Optionally redirect or perform additional actions
             return HttpResponseRedirect(".")
 
@@ -117,6 +143,13 @@ class StatisticsAdmin(admin.ModelAdmin):
             'total_paid': paid_orders_sum,
             'total_unpaid': unpaid_orders_sum,
         })
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        obj = self.get_object(request, object_id)
+        if extra_context is None:
+            extra_context = {}
+        extra_context['object'] = obj
+        return super().change_view(request, object_id, form_url, extra_context=extra_context)
 
 
 admin.site.register(Statistics, StatisticsAdmin)
--- apps/orders/models/statistics.py
+++ apps/orders/models/statistics.py
@@ -6,7 +6,8 @@ from django.db.models import Sum
 
 from apps.orders.models import Order
 
-import datetime, calendar
+import datetime
+import calendar
 from datetime import timedelta
 
 
@@ -14,8 +15,9 @@ class StatisticsManager(models.Manager):
 
     def calculate_per_waitress(self, date=None):
         if not date:
-            date = timezone.localdate() - timedelta(days=1)  # Default to today if no date is provided
-        
+            # Default to today if no date is provided
+            date = timezone.localdate() - timedelta(days=1)
+
         waitresses_orders = Order.objects.filter(
             created_at__date=date,
             is_paid=True,  # Considering only paid orders
@@ -36,16 +38,20 @@ class StatisticsManager(models.Manager):
 
     def calculate_daily(self, date=None):
         if not date:
-            date = timezone.localdate() - timedelta(days=1) # Default to today if no date is provided
+            # Default to today if no date is provided
+            date = timezone.localdate() - timedelta(days=1)
 
         self.calculate_per_waitress(date=date)
 
         existing_stat = self.filter(title='daily', date=date).first()
         if not existing_stat:
-            start_of_day = timezone.make_aware(datetime.datetime.combine(date, datetime.time.min))
-            end_of_day = timezone.make_aware(datetime.datetime.combine(date, datetime.time.max))
+            start_of_day = timezone.make_aware(
+                datetime.datetime.combine(date, datetime.time.min))
+            end_of_day = timezone.make_aware(
+                datetime.datetime.combine(date, datetime.time.max))
 
-            daily_total = Order.objects.filter(created_at__range=(start_of_day, end_of_day), is_paid=True).aggregate(Sum('total_price'))['total_price__sum'] or 0
+            daily_total = Order.objects.filter(created_at__range=(
+                start_of_day, end_of_day), is_paid=True).aggregate(Sum('total_price'))['total_price__sum'] or 0
 
             if not daily_total:
                 return
@@ -60,18 +66,21 @@ class StatisticsManager(models.Manager):
 
     def calculate_monthly(self, date=None):
         if not date:
-            date = timezone.localdate() - timedelta(days=1) # Default to this month if no date is provided
+            # Default to this month if no date is provided
+            date = timezone.localdate() - timedelta(days=1)
         first_of_month = date.replace(day=1)
-        last_day = date.replace(day=calendar.monthrange(date.year, date.month)[1])
+        last_day = date.replace(
+            day=calendar.monthrange(date.year, date.month)[1])
 
         for single_date in (first_of_month + datetime.timedelta(days=n) for n in range((last_day - first_of_month).days + 1)):
             self.calculate_daily(single_date)
 
-        monthly_total = self.filter(title='daily', date__range=[first_of_month, last_day]).aggregate(Sum('total'))['total__sum'] or 0
-        
+        monthly_total = self.filter(title='daily', date__range=[
+                                    first_of_month, last_day]).aggregate(Sum('total'))['total__sum'] or 0
+
         if not monthly_total:
             return
-        
+
         monthly_stat, created = self.update_or_create(
             title='monthly',
             date=first_of_month,
@@ -81,7 +90,8 @@ class StatisticsManager(models.Manager):
 
     def calculate_yearly(self, date=None):
         if not date:
-            date = timezone.localdate() - timedelta(days=1) # Default to this year if no date is provided
+            # Default to this year if no date is provided
+            date = timezone.localdate() - timedelta(days=1)
         first_of_year = date.replace(month=1, day=1)
         last_month = date.month
 
@@ -89,11 +99,12 @@ class StatisticsManager(models.Manager):
             month_date = first_of_year.replace(month=month)
             self.calculate_monthly(month_date)
 
-        yearly_total = self.filter(title='monthly', date__year=date.year).aggregate(Sum('total'))['total__sum'] or 0
+        yearly_total = self.filter(title='monthly', date__year=date.year).aggregate(
+            Sum('total'))['total__sum'] or 0
 
         if not yearly_total:
             return
-        
+
         yearly_stat, created = self.update_or_create(
             title='yearly',
             date=first_of_year,
@@ -101,22 +112,50 @@ class StatisticsManager(models.Manager):
         )
         return yearly_stat
 
+    def calculate_till_now(self):
+        till_now_total = Order.objects.filter(is_paid=True).aggregate(
+            Sum('total_price'))['total_price__sum'] or 0
+
+        if not till_now_total:
+            return
+
+        till_now_stat, created = self.update_or_create(
+            title='till_now',
+            date=timezone.localdate(),
+            defaults={'total': till_now_total}
+        )
+        return till_now_stat
+
     def delete_orders_for_statistics_day(self, date):
         # First check if statistics exist for the day, if not calculate them
         self.calculate_daily(date)
 
-        start_of_day = timezone.make_aware(datetime.datetime.combine(date, datetime.time.min))
-        end_of_day = timezone.make_aware(datetime.datetime.combine(date, datetime.time.max))
+        start_of_day = timezone.make_aware(
+            datetime.datetime.combine(date, datetime.time.min))
+        end_of_day = timezone.make_aware(
+            datetime.datetime.combine(date, datetime.time.max))
 
         # Filter and delete orders within the specified range that are paid
-        orders = Order.objects.filter(created_at__range=(start_of_day, end_of_day), is_paid=True)
+        orders = Order.objects.filter(created_at__range=(
+            start_of_day, end_of_day), is_paid=True)
         count = orders.count()  # Count orders before deletion for reporting
         orders.delete()
         return count  # Return the number of deleted orders for confirmation/logging
 
+    def delete_orders_till_now(self):
+        # Calculate the till_now statistic if it doesn't exist
+        self.calculate_till_now()
+
+        # Filter and delete all paid orders up to the current date
+        orders = Order.objects.filter(is_paid=True)
+        count = orders.count()  # Count orders before deletion for reporting
+        orders.delete()
+        return count
+
 
 class Statistics(DateTimeModel, models.Model):
     TITLE_CHOICES = (
+        ("till_now", "Bu günə kimi"),
         ("daily", "Günlük"),
         ("monthly", "Aylıq"),
         ("yearly", "İllik"),
@@ -129,11 +168,12 @@ class Statistics(DateTimeModel, models.Model):
         default=TITLE_CHOICES[0][0],
         verbose_name="Başlıq"
     )
-    total = models.DecimalField(max_digits=10, decimal_places=2, default=0, verbose_name="Ümumi")
+    total = models.DecimalField(
+        max_digits=10, decimal_places=2, default=0, verbose_name="Ümumi")
     date = models.DateField(default=timezone.now, verbose_name="Tarix")
 
     waitress_info = models.CharField(
-        max_length=90, blank=True, null=True, 
+        max_length=90, blank=True, null=True,
         help_text="Ofisiant kodu və adı.",
         verbose_name="Ofisiant"
     )
