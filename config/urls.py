b8fc8965ab33610b430f2278ee373475da2ac9d5
Zakir Aghayev <agayevzakir239@gmail.com>
2024-06-10 23:01:14 +0400

    stollar

--- config/urls.py
+++ config/urls.py
@@ -36,9 +36,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('backend/admin/', admin.site.urls),
     path('api/', include('apps.urls')),
-    path('backend/api/', include('apps.urls')),
 
 
     # SWAGGER
