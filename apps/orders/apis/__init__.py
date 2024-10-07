3b29dbd150c05a719f2321141e0c9ef9120f5bf0
Zakir Aghayev <zakir.aghayev.19@gmail.com>
2024-10-07 19:37:11 +0400

    Remove order item

--- apps/orders/apis/__init__.py
+++ apps/orders/apis/__init__.py
@@ -3,8 +3,8 @@ from apps.orders.apis.orders import CheckOrderAPIView
 from apps.orders.apis.orders import AddOrderItemAPIView
 from apps.orders.apis.orders import AddMultipleOrderItemsAPIView
 from apps.orders.apis.orders import ListOrderItemsAPIView
-from apps.orders.apis.managers import CloseTableOrderAPIView
 
+from apps.orders.apis.managers import CloseTableOrderAPIView
 from apps.orders.apis.managers import DeleteOrderItemAPIView
 from apps.orders.apis.managers import ChangeOrderTableAPIView
 from apps.orders.apis.managers import ListWaitressAPIView
