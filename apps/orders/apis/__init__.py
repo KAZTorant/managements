fb353302088311af323479a59032e02b04b4b9c7
Zakir Aghayev <zakir.aghayev.19@gmail.com>
2024-10-07 19:48:26 +0400

    imported

--- apps/orders/apis/__init__.py
+++ apps/orders/apis/__init__.py
@@ -1,13 +1,15 @@
-from apps.orders.apis.orders import CreateOrderAPIView
-from apps.orders.apis.orders import CheckOrderAPIView
-from apps.orders.apis.orders import AddOrderItemAPIView
-from apps.orders.apis.orders import AddMultipleOrderItemsAPIView
-from apps.orders.apis.orders import ListOrderItemsAPIView
 
-from apps.orders.apis.managers import CloseTableOrderAPIView
-from apps.orders.apis.managers import DeleteOrderItemAPIView
-from apps.orders.apis.managers import ChangeOrderTableAPIView
-from apps.orders.apis.managers import ListWaitressAPIView
-from apps.orders.apis.managers import ChangeWaitressAPIView
+from apps.orders.apis.order_items.add import AddOrderItemAPIView
+from apps.orders.apis.order_items.list import ListOrderItemsAPIView
+from apps.orders.apis.order_items.remove import DeleteOrderItemAPIView
+
+from apps.orders.apis.orders.create import CreateOrderAPIView
+from apps.orders.apis.orders.check import CheckOrderAPIView
+from apps.orders.apis.orders.close import CloseTableOrderAPIView
+from apps.orders.apis.orders.change_table import ChangeOrderTableAPIView
+from apps.orders.apis.orders.list_waitress import ListWaitressAPIView
+from apps.orders.apis.orders.change_waitress import ChangeWaitressAPIView
 
 from apps.orders.apis.printer import PrintCheckAPIView
+
+# from apps.orders.apis.order_items. import AddMultipleOrderItemsAPIView
