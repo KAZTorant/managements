6573f7ec04d52568b8848cef3809ae6c67999651
Zakir Aghayev <zakir.aghayev.19@gmail.com>
2025-03-07 19:34:53 +0400

    FilterAdded

--- apps/finance/views.py
+++ apps/finance/views.py
@@ -1,42 +1,59 @@
 from rest_framework import generics
 from django.db.models import Sum
 from rest_framework.response import Response
-from datetime import date
+from datetime import date, timedelta
 
 from apps.finance.models import Income, Expense
 from apps.finance.serializers import IncomeSerializer, ExpenseSerializer
 
+
 class IncomeCreateView(generics.CreateAPIView):
-    queryset = Income.objects.all()
+    start_date = date.today() - timedelta(days=30)
+    queryset = Income.objects.filter(date__gte=start_date)
     serializer_class = IncomeSerializer
+
 
 class ExpenseCreateView(generics.CreateAPIView):
-    queryset = Expense.objects.all()
+    start_date = date.today() - timedelta(days=30)
+    queryset = Expense.objects.filter(date__gte=start_date)
     serializer_class = ExpenseSerializer
+
 
 class IncomeListView(generics.ListAPIView):
-    queryset = Income.objects.all()
+    start_date = date.today() - timedelta(days=30)
+    queryset = Income.objects.filter(date__gte=start_date)
     serializer_class = IncomeSerializer
 
+
 class ExpenseListView(generics.ListAPIView):
-    queryset = Expense.objects.all()
+    start_date = date.today() - timedelta(days=30)
+    queryset = Expense.objects.filter(date__gte=start_date)
     serializer_class = ExpenseSerializer
+
 
 class DailyResultView(generics.GenericAPIView):
     def get(self, request):
         today = date.today()
-        total_income = Income.objects.filter(date=today).aggregate(Sum('amount'))['amount__sum'] or 0
-        total_expense = Expense.objects.filter(date=today).aggregate(Sum('amount'))['amount__sum'] or 0
+        total_income = Income.objects.filter(date=today).aggregate(Sum('amount'))[
+            'amount__sum'] or 0
+        total_expense = Expense.objects.filter(date=today).aggregate(Sum('amount'))[
+            'amount__sum'] or 0
         result = total_income - total_expense
         return Response({"date": today, "total_income": total_income, "total_expense": total_expense, "result": result})
+
 
 class AllResultsView(generics.GenericAPIView):
     def get(self, request):
         results = []
-        unique_dates = Income.objects.values_list('date', flat=True).distinct()
+        start_date = date.today() - timedelta(days=30)
+        unique_dates = Income.objects.filter(
+            date__gte=start_date).values_list('date', flat=True).distinct()
         for day in unique_dates:
-            total_income = Income.objects.filter(date=day).aggregate(Sum('amount'))['amount__sum'] or 0
-            total_expense = Expense.objects.filter(date=day).aggregate(Sum('amount'))['amount__sum'] or 0
+            total_income = Income.objects.filter(date=day).aggregate(Sum('amount'))[
+                'amount__sum'] or 0
+            total_expense = Expense.objects.filter(date=day).aggregate(Sum('amount'))[
+                'amount__sum'] or 0
             result = total_income - total_expense
-            results.append({"date": day, "total_income": total_income, "total_expense": total_expense, "result": result})
+            results.append({"date": day, "total_income": total_income,
+                           "total_expense": total_expense, "result": result})
         return Response(results)
